0cfb8bbe92572cd670c75c135b4ede5db8b7c044
Christopher Bergqvist <chris@digitalpoetry.se>
2020-07-17 21:21:52 +0200

    Grab front matter title from file name in more sophisticated way

--- src/front_matter.rs
+++ src/front_matter.rs
@@ -118,7 +118,16 @@ pub fn parse(
 		}
 	}
 
-	fixup_date(input_file_path, &mut result);
+	let file_stem = input_file_path
+		.file_stem()
+		.unwrap_or_else(|| {
+			panic!("Missing file stem on: {}", input_file_path.display())
+		})
+		.to_string_lossy();
+
+	let file_name_date = get_file_name_date(&file_stem);
+	fixup_date(input_file_path, &file_name_date, &mut result);
+	fixup_title(input_file_path, &file_stem, &file_name_date, &mut result);
 
 	if !result.published
 		&& input_file_path.extension()
@@ -130,12 +139,46 @@ pub fn parse(
 	result
 }
 
-fn fixup_date(input_file_path: &PathBuf, front_matter: &mut FrontMatter) {
+fn get_file_name_date(file_stem: &str) -> Option<String> {
+	let pattern = "####-##-##_";
+
+	let mut matching_chars = 0;
+	for (c, p) in file_stem.chars().zip(pattern.chars()) {
+		if !match p {
+			'#' => c >= '0' && c <= '9',
+			_ => c == p,
+		} {
+			break;
+		}
+		matching_chars += 1;
+	}
+
+	if matching_chars == pattern.len() {
+		Some(file_stem[..pattern.len() - 1].to_string())
+	} else {
+		None
+	}
+}
+
+fn fixup_date(
+	input_file_path: &PathBuf,
+	file_name_date: &Option<String>,
+	front_matter: &mut FrontMatter,
+) {
 	if let Some(date) = &front_matter.date {
 		if date != "auto" {
+			if let Some(file_name_date) = file_name_date {
+				if date.len() >= file_name_date.len()
+					&& &date[..file_name_date.len()] != file_name_date
+				{
+					panic!("Expected date in front matter of {} to start with {} but it is: {}",
+						input_file_path.display(), file_name_date, date);
+				}
+			}
 			if let Some(edited) = &front_matter.edited {
 				if edited != "auto" {
-					// If neither date nor edited are set to "auto", bail.
+					// If date and edited are both set, but none of them is
+					// "auto", bail.
 					return;
 				}
 			} else {
@@ -163,18 +206,52 @@ fn fixup_date(input_file_path: &PathBuf, front_matter: &mut FrontMatter) {
 		)
 	});
 
-	let fs_time = Some(humantime::format_rfc3339_seconds(modified).to_string());
+	let fs_time = humantime::format_rfc3339_seconds(modified).to_string();
 
 	if front_matter.date.as_deref() == Some("auto") {
-		front_matter.date = fs_time;
+		if let Some(file_name_date) = file_name_date {
+			if &fs_time[..file_name_date.len()] != file_name_date {
+				panic!(
+					"Expected file system time of {} to match {} but it is: {}",
+					input_file_path.display(),
+					file_name_date,
+					fs_time
+				);
+			}
+		}
+
+		front_matter.date = Some(fs_time);
 		if front_matter.edited.is_some() {
 			panic!("Can't have date set to \"auto\" while also specifying edited in front matter of {}", input_file_path.display());
 		}
 	} else if (front_matter.date.is_none() && front_matter.edited.is_none())
 		|| front_matter.edited.as_deref() == Some("auto")
 	{
-		front_matter.edited = fs_time;
+		front_matter.edited = Some(fs_time);
 	}
+}
+
+fn fixup_title(
+	input_file_path: &PathBuf,
+	file_stem: &str,
+	file_name_date: &Option<String>,
+	front_matter: &mut FrontMatter,
+) {
+	if !front_matter.title.is_empty()
+		|| input_file_path.extension()
+			!= Some(OsStr::new(util::MARKDOWN_EXTENSION))
+	{
+		return;
+	}
+
+	front_matter.title = util::capitalize(
+		&if let Some(file_name_date) = file_name_date {
+			&file_stem[file_name_date.len() + 1..]
+		} else {
+			file_stem
+		}
+		.replace("_", " "),
+	);
 }
 
 fn parse_yaml_attribute(
